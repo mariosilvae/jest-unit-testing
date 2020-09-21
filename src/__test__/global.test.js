1d4191a8a72ddb003bcfeb6dde343b188f76de9b
Mario Silva <mario.silvae@outlook.com>
2020-09-21 08:19:02 -0500

    testing booleans, arrays, numbers and callbacks

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,6 +1,29 @@
 const { TestScheduler } = require("jest");
 const text = "Hello World";
+const fruits = ["apple", "banana", "watermelon"];
 
+//String test
 test("It should has the word World", () => {
   expect(text).toMatch(/World/);
+});
+
+//Array test
+test("It contain banana?", () => {
+  expect(fruits).toContain("banana");
+});
+
+//Number test
+test("Greater than", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+//Callback test
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""));
+};
+
+test("Test a callback", () => {
+  reverseString("hello", (str) => {
+    expect(str).toBe("olleh");
+  });
 });
